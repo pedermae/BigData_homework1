cae12ff3474242a6aa38436e3f4928cb3961d05b
Durval <pedrocruzeiro@ua.pt>
2024-05-02 16:28:34 +0200

    file name corrections

--- G049HW2.py
+++ G049HW2.py
@@ -1,53 +1,58 @@
-
-from pyspark import SparkContext, SparkConf, RDD
+from pyspark import SparkContext, SparkConf
 from math import sqrt, floor
-import sys, os, time, zipfile
+import sys, os, time
 import random as rand        
 
-
+        
 def MRApproxOutliers(points_RDD, D, M):
-    
-    #Step A
-    Lambda = D / (2*sqrt(2))
-    
-    def map_to_cells(point):
+
+    def map_to_cell(point):
+        Lambda = D / (2*sqrt(2))
         i = floor(point[0]/Lambda)
         j = floor(point[1]/Lambda)
-        return ((i,j), 1)
+        return (i,j)
     
-    cells_RDD = points_RDD.map(map_to_cells).reduceByKey(lambda a, b: a + b)
+    #Assign each point to a cell
+    cells_RDD = points_RDD.map(lambda point: (map_to_cell(point), 1)).reduceByKey(lambda a , b: a + b)
+    local_cells = cells_RDD.collectAsMap()
     
-    local_cells = cells_RDD.collectAsMap() #Think we can download locally here, not sure if already done in caching in Main.
+    #Calculate N3 and N7 for each cell
+    count_N3N7 = {}
+    for cell in local_cells.items():
+        count_N3N7[cell[0]] = [cell[1], 0, 0]
+        
+        for i in range(-3, 4):
+            for j in range(-3, 4):
+                cell_ij = (cell[0][0] + i, cell[0][1] + j)
+                if cell_ij in local_cells:
+                    cell_count = local_cells[cell_ij]
+                    if (i < -1 or i > 1) or (j < -1 or j > 1):
+                        #In C7
+                        count_N3N7[cell[0]][2] += cell_count
+                    else:                                   
+                        #In C3
+                        count_N3N7[cell[0]][2] += cell_count
+                        count_N3N7[cell[0]][1] += cell_count
 
-    def calculate_N3_N7(cell):
-        i, j = cell[0]
-        N3 = sum([local_cells.get((i+di, j+dj), 0) for di in range(-1, 2) for dj in range(-1, 2)])
-        N7 = sum([local_cells.get((i+di, j+dj), 0) for di in range(-3, 4) for dj in range(-3, 4)])
-        return (cell[0], (cell[1], N3, N7))
+        #Calculate outliers
+        sure_outliers = 0
+        uncertain_outliers = 0
+        for cell, counts in count_N3N7.items():
+            if counts[2] <= M:
+                sure_outliers += counts[0]
+            
+            if counts[1] <= M and counts[2] > M:
+                uncertain_outliers += counts[0]
+    print(f"Sure outliers: {sure_outliers}")
+    print(f"Uncertain outliers: {uncertain_outliers}")
         
-    cells_with_N3_N7 = cells_RDD.map(calculate_N3_N7)
-    
-    
-    #Structure of x : ((i,j), (count, N3, N7))
-    non_outliers = cells_with_N3_N7.filter(lambda x: x[1][1] > M).count()
-    sure_outliers = cells_with_N3_N7.filter(lambda x: x[1][2] <= M).count()
-    uncertain_outliers = cells_with_N3_N7.filter(lambda x: x[1][1] <= M and x[1][2] > M).count()
-    ordered_cells = cells_RDD.sortBy(lambda x: x[1], ascending=True)
-    
-    print("Number of sure (D,M)-outliers: ", sure_outliers)
-    print("Number of uncertain points: ", uncertain_outliers)
-    print("Number of non outlier points: ", non_outliers)
-        
-    for cell, size in ordered_cells:
-        print(f"Cell: {cell}, Size: {size}")
-        
+
         
 def eucl_dist(p1,p2):
         return (((p1[0] - p2[0])**2) + (p1[1]-p2[1])**2)**0.5
         
 
 def SequentialFFT(P: list, K: int) -> list:
-    print("P: ", P)
     try:
         S = [P.pop(rand.randint(0, len(P) - 1))]
     except ValueError:
@@ -58,7 +63,6 @@ def SequentialFFT(P: list, K: int) -> list:
         S.append(point)
         P.remove(point)
     
-    print("S: ", S)
     return S
 
         
@@ -73,15 +77,13 @@ def main():
     assert os.path.isfile(file_name), "File or folder not found"
     M, K, L = int(M), int(K), int(L)
     print(f"data path: {file_name}, M: {M}, K: {K}, L: {L}")
-    
-
             
     conf = SparkConf().setAppName('HW2')
     sc = SparkContext(conf=conf)
     
-    point_map = []
-    rawData = sc.textFile(file_name).repartition(L).cache() #L is num of partitions
+    rawData = sc.textFile(file_name) #L is num of partitions
     inputPoints = rawData.map(lambda line: tuple(map(float, line.split(','))))
+    inputPoints = inputPoints.repartition(L).cache()
     points_num = inputPoints.count()
     print(f"Number of points = {points_num}")
     round1 = inputPoints.mapPartitions(lambda partition: SequentialFFT(list(partition), K))
